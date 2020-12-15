fc9c0b216fbb0597bccfde7ee0db809456b14b47
techClubMember <jesserussell411@gmail.com>
2020-12-15 00:06:22 -0700

    Add project files.

---
+++ StopWatch/Debugging.cpp
@@ -0,0 +1,27 @@
+#include "Stopwatch.h"
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+using namespace std;
+using namespace JesseRussell::Diagnostics;
+
+int main() {
+	Stopwatch s;
+	cout << s.getElapsedMilliseconds() << endl;
+	s.start();
+	this_thread::sleep_for(chrono::milliseconds(200));
+	s.stop();
+	cout << s.getElapsedMilliseconds() << endl;
+
+	s.reset();
+	s.start();
+	this_thread::sleep_for(chrono::milliseconds(100));
+	cout << s.getElapsedMilliseconds() << endl;
+
+	s.restart();
+	this_thread::sleep_for(chrono::milliseconds(300));
+	cout << s.getElapsedMilliseconds() << endl;
+	this_thread::sleep_for(chrono::milliseconds(400));
+	cout << s.getElapsedMilliseconds() << endl;
+}
---
+++ StopWatch/Stopwatch.h
@@ -0,0 +1,56 @@
+#pragma once
+#include<chrono>
+
+namespace JesseRussell {
+	namespace Diagnostics {
+		class Stopwatch
+		{
+		private:
+			std::chrono::steady_clock::time_point startTime, stopTime;
+			bool running, ready;
+
+		public:
+			Stopwatch() {
+				reset();
+			}
+
+			void start() {
+				if (ready) {
+					startTime = std::chrono::high_resolution_clock::now();
+					ready = false;
+				}
+				running = true;
+			}
+
+			void stop() {
+				if (running) {
+					stopTime = std::chrono::high_resolution_clock::now();
+					running = false;
+				}
+			}
+
+			void reset() {
+				running = false;
+				ready = true;
+			}
+
+			void restart() {
+				reset();
+				start();
+			}
+
+			std::chrono::steady_clock::duration getElapsed() {
+				if (running)
+					return std::chrono::high_resolution_clock::now() - startTime;
+				else if (ready)
+					return std::chrono::steady_clock::duration::zero();
+				else
+					return stopTime - startTime;
+			}
+
+			long long getElapsedMilliseconds() {
+				return std::chrono::duration_cast<std::chrono::milliseconds>(getElapsed()).count();
+			}
+		};
+	}
+}
